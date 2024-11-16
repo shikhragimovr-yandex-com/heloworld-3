23805c95a7354859ec5b94297298c8e9558769c6
Админ <shikhragimovr@yandex.ru>
2024-11-16 16:19:15 +0500

    сделано дз 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("привет мир");
         byte apples = 5;
         short nuts = 200;
         int rubles = 10000;
